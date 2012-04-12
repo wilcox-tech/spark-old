3f39b47eb4264d8e96f25d7b7746a1054cba357f
Ricky Elrod <ricky@elrod.me>
2012-04-12 06:28:26 -0400

    Start using Twitter Bootstrap!

--- app/assets/javascripts/setup.js
+++ app/assets/javascripts/setup.js
@@ -1,7 +1,16 @@
-// Because relrod says I should do it this way.
 $(function() {
   $("#ldap-auth").hide();
   $("#db-auth").hide();
-  $("#db_auth_true").click(function() { $("#ldap-auth").hide(); $("#db-auth").show(); } )
-  $("#db_auth_false").click(function() {$("#ldap-auth").show(); $("#db-auth").hide(); } )
+  $("#submitform").hide();
+  $("#db_auth_true").click(function() {
+    $("#ldap-auth").hide();
+    $("#db-auth").show();
+    $("#submitform").show();
+  });
+
+  $("#db_auth_false").click(function() {
+    $("#ldap-auth").show();
+    $("#db-auth").hide();
+    $("#submitform").show();
+  });      
 });
